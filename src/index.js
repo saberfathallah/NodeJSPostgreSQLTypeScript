dcb34ccc86dcbf7034a54ea3cff44d632d987294
Saber Fathallah <saberfathallah7@gmail.com>
2020-05-08 16:01:21 +0200

    apis (#1)

--- src/index.js
+++ src/index.js
@@ -14,14 +14,42 @@ const app = express();
 
 app.use(bodyParser.json());
 
-
-app.get('/', (req, res) => res.json({ msg: 'dssdddd' }));
-
 app.post('/users', async (req, res) => {
   const args = req.body;
   try {
     const user = await models.User.create(args);
     res.json({ user });
+  } catch (error) {
+    console.log('error ', error);
+  }
+});
+
+app.post('/categories', async (req, res) => {
+  const args = req.body;
+  try {
+    const category = await models.Category.create(args);
+    res.json({ category });
+  } catch (error) {
+    console.log('error ', error);
+  }
+});
+
+app.delete('/categories/:id', async (req, res) => {
+  try {
+    await models.Category.destroy({
+      where: { id: req.params.id },
+    });
+    res.json(`category deleted with id ${req.params.id}`);
+  } catch (error) {
+    console.log('error ', error);
+  }
+});
+
+app.post('/posts', async (req, res) => {
+  const args = req.body;
+  try {
+    const post = await models.Post.create(args);
+    res.json({ post });
   } catch (error) {
     console.log('error ', error);
   }
